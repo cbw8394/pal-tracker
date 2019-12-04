af834eff6db3c80b3ba534dbd26a3abd129cec11
pal_user <cbw8394@gmail.com>
2019-12-04 08:14:33 -0700

    jdbcrepo fix error

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import javax.sql.DataSource;
+
 @SpringBootApplication
 public class PalTrackerApplication {
 
@@ -13,9 +15,7 @@ public class PalTrackerApplication {
     }
 
     @Bean
-    public JdbcTimeEntryRepository getTimeEntryRepository(){
-        MysqlDataSource dataSource = new MysqlDataSource();
-        dataSource.setUrl(System.getenv("SPRING_DATASOURCE_URL"));
+    public JdbcTimeEntryRepository getTimeEntryRepository(DataSource dataSource){
 
         return new JdbcTimeEntryRepository(dataSource);
     }
